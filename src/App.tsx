ee3969e66d6f320c239917a0c0d723f2c0e0285e
heosuyeon <hsy121891@gmail.com>
2024-12-24 14:01:52 +0900

    절대경로 적용

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,25 @@
 import { BrowserRouter, Route, Routes } from "react-router";
-import './App.css'
-import Home from "./pages/Home";
-import Work from "./pages/Work";
-import Solution from "./pages/Solution";
-import Contact from "./pages/Contact";
+import "@/App.css";
+import Home from "@/pages/Home";
+import Work from "@/pages/Work";
+import Solution from "@/pages/Solution";
+import Contact from "@/pages/Contact";
+import Header from "@/components/layout/Header";
+import Footer from "@/components/layout/Footer";
 
 function App() {
- 
-  return (
-    <BrowserRouter>
-      <Routes>
-        <Route index element={<Home />} />
-        <Route path="work" element={<Work />} />
-        <Route path="solution" element={<Solution />} />
-        <Route path="contact" element={<Contact />} />
-      </Routes>
-    </BrowserRouter>
-  )
+    return (
+        <BrowserRouter>
+            <Header />
+            <Routes>
+                <Route index element={<Home />} />
+                <Route path="work" element={<Work />} />
+                <Route path="solution" element={<Solution />} />
+                <Route path="contact" element={<Contact />} />
+            </Routes>
+            <Footer />
+        </BrowserRouter>
+    );
 }
 
-export default App
+export default App;
